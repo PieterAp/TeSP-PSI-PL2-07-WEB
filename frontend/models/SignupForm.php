148efb550d8f6895c26bbaf1c93b065159d38c13
pedromig123 <pedropedro_miguel@hotmail.com>
2018-11-06 18:36:39 +0000

    signin/signup/useraccount

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -8,8 +8,14 @@ use common\models\User;
  * Signup form
  */
 class SignupForm extends Model
+
 {
     public $username;
+    public $firstname;
+    public $lastname;
+    public $NIF;
+    public $dataNasc;
+    public $morada;
     public $email;
     public $password;
 
@@ -24,6 +30,26 @@ class SignupForm extends Model
             ['username', 'required'],
             ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['firstname', 'trim'],
+            ['firstname', 'required'],
+            ['firstname', 'string', 'min' => 3, 'max' => 16],
+
+            ['lastname', 'trim'],
+            ['lastname', 'required'],
+            ['lastname', 'string', 'min' => 3, 'max' => 16],
+
+            ['NIF', 'trim'],
+            ['NIF', 'required'],
+            ['NIF', 'number', 'min' => 9, 'max' => 9],
+
+            ['dataNasc', 'trim'],
+            ['dataNasc', 'required'],
+            ['dataNasc', 'date', 'format' => 'php:Y-m-d'],
+
+            ['morada', 'trim'],
+            ['morada', 'required'],
+            ['morada', 'string', 'min' => 9, 'max' => 255],
 
             ['email', 'trim'],
             ['email', 'required'],
@@ -43,16 +69,29 @@ class SignupForm extends Model
      */
     public function signup()
     {
-        if (!$this->validate()) {
-            return null;
+        if ($this->validate()) {
+            
+            $user = new User();
+            $userData = new userData();
+            
+            $user->username = $this->username;
+            $user->email = $this->email;
+            $user->setPassword($this->password);
+
+            $userData->firstname = ($this->firstname);
+            $userData->lastname = ($this->lastname);
+            $userData->NIF = ($this->NIF);
+            $userData->dataNasc = ($this->dataNasc);
+            $userData->morada = ($this->dataNasc);
+            $userData->save();
+
+            $user->generateAuthKey();
+            $auth = \Yii::$app->authManager;
+            $authorRole = $auth->getRole('cliente');
+            $auth->assign($authorRole, $user->getId());
+            return $user->save() ? $user : null;
+
         }
-        
-        $user = new User();
-        $user->username = $this->username;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->generateAuthKey();
-        
-        return $user->save() ? $user : null;
+        return null;
     }
 }
