bbb9402fb261142f417f1865482cf295472d9032
PieterAp <2170365@my.ipleiria.pt>
2018-11-16 17:24:26 +0000

    Created migration m181115_094516_create_table_categoria_child.php

---
+++ console/migrations/m181115_094516_create_table_categoria_child.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m181115_094516_create_table_categoria_child extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%categoria_child}}', [
+            'idchild' => $this->primaryKey(),
+            'childNome' => $this->string()->notNull(),
+            'childDescricao' => $this->string(),
+            'categoria_idcategorias' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex('fk_categoria_child_categoria1_idx', '{{%categoria_child}}', 'categoria_idcategorias');
+        $this->addForeignKey('fk_categoria_child_categoria1', '{{%categoria_child}}', 'categoria_idcategorias', '{{%categoria}}', 'idcategorias', 'NO ACTION', 'NO ACTION');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%categoria_child}}');
+    }
+}
