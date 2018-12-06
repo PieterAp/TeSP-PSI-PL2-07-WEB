d05d96b9571c9bbd5707082ff5a55916bf0a8619
PieterAp <2170365@my.ipleiria.pt>
2018-12-06 05:42:58 +0000

    Update m181125_162407_createDefaultTableData.php

--- console/migrations/m181125_162407_createDefaultTableData.php
+++ console/migrations/m181125_162407_createDefaultTableData.php
@@ -1,6 +1,9 @@
 <?php
 
 use common\models\Campanha;
+use common\models\Categoria;
+use common\models\CategoriaChild;
+use common\models\Produto;
 use yii\db\Migration;
 use common\models\User;
 use common\models\Userdata;
@@ -48,21 +51,31 @@ class m181125_162407_createDefaultTableData extends Migration
     }
 
 
-/*    public function up()
+    public function up()
     {
         //region table_user
+
+        $myfile = fopen("users.txt", "w") or print_r("Unable to open file!");
+
         //region Role:admin
         //region Username:pieter Role:admin
         $user = $this->createUser('pieter','pieter@gmail.com','pieterpieter');
         $this->createUserData($user,'Pieter','Aparicio','245875326','1997-05-08','Avenida Humberto Delgado, Leiria');
         $this->givePermission($user,'admin');
+
+        $txt = "ADMIN Username: ".$user->username." Password: pieterpieter"."\n";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
 
         //region Username:pedro Role:admin
         $user = $this->createUser('pedro','pedro@gmail.com','pedropedro');
         $this->createUserData($user,'Pedro','Inácio','245875326','1997-09-08','Rua António Frade, Marinha Grande');
         $this->givePermission($user,'admin');
+
+        $txt = "ADMIN Username: ".$user->username." Password: pedropedro";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
+
         //endregion
 
         //region Role:funcionarios
@@ -70,12 +83,18 @@ class m181125_162407_createDefaultTableData extends Migration
         $user = $this->createUser('david','david@gmail.com','daviddavid');
         $this->createUserData($user,'David','Sousa','245875326','1995-09-08','Rua Humberto Delgado, Lisboa');
         $this->givePermission($user,'funcionario');
+
+        $txt = "FUNCIONARIO Username: ".$user->username." Password: daviddavid";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
 
         //region Username:tiago Role:funcionario
         $user = $this->createUser('tiago','tiago@gmail.com','tiagotiago');
         $this->createUserData($user,'Tiago','Mendes','245875326','1995-09-08','Rua Humberto Delgado, Lisboa');
         $this->givePermission($user,'funcionario');
+
+        $txt = "FUNCIONARIO Username: ".$user->username." Password: tiagotiago";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
         //endregion
 
@@ -84,23 +103,31 @@ class m181125_162407_createDefaultTableData extends Migration
         $user = $this->createUser('margarida','margarida@gmail.com','margaridamargarida');
         $this->createUserData($user,'Margarida','Isabel','245875326','1997-02-14','Rua Humberto Delgado, Rio Maior');
         $this->givePermission($user,'cliente');
+
+        $txt = "CLIENTE Username: ".$user->username." Password: margaridamargarida";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
 
         //region Username:joao Role:cliente
         $user = $this->createUser('joao','joao@gmail.com','joaojoao');
         $this->createUserData($user,'João','Almeida','245875326','1997-09-06','Rua Humberto Delgado, Lisboa');
         $this->givePermission($user,'cliente');
+
+        $txt = "CLIENTE Username: ".$user->username." Password: joaojoao";
+        $myfile = file_put_contents('users.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
         //endregion
         //endregion
+
+        echo "\n * * * User information has been written to file users.txt in the root of this project! * * * \n";
         //endregion
 
 
         //region table_campanha
-        //region Campanha: Saldos de natal
+        //region Campanha: Christmas
         $campanha = new Campanha();
-        $campanha->campanhaNome = 'Saldos de Natal';
+        $campanha->campanhaNome = 'Christmas';
         $campanha->campanhaDataInicio = '2018-12-01';
-        $campanha->campanhaDescricao = 'Produtos em desconto durante a época de natal';
+        $campanha->campanhaDescricao = 'All the best for your family and friends for a pleasent holiday!';
         $campanha->campanhaDataFim = '2018-12-25';
         $campanha->save();
         //endregion
@@ -109,27 +136,191 @@ class m181125_162407_createDefaultTableData extends Migration
         $campanha = new Campanha();
         $campanha->campanhaNome = 'Black Weekend';
         $campanha->campanhaDataInicio = '2018-12-15';
-        $campanha->campanhaDescricao = 'Produtos em desconto durante o Black Weekend';
+        $campanha->campanhaDescricao = 'Get the stuff you always wantd for the lowest price!';
         $campanha->campanhaDataFim = '2018-12-16';
+        $campanha->save();
+        //endregion
+
+        //region Campanha: Anniversary of FixByte
+        $campanha = new Campanha();
+        $campanha->campanhaNome = 'Anniversary of FixByte';
+        $campanha->campanhaDataInicio = '2018-12-01';
+        $campanha->campanhaDescricao = 'To celebrate the birth of FixByte!';
+        $campanha->campanhaDataFim = '2018-12-31';
         $campanha->save();
         //endregion
         //endregion
 
 
         //region table_categoria
+        $campanha = new Categoria();
+        $campanha->categoriaNome = 'Components';
+        $campanha->categoriaDescricao = 'Usually used to build your own computer';
+        $campanha->categoriaEstado = 1;
+        $campanha->save();
 
+        $campanha = new Categoria();
+        $campanha->categoriaNome = 'Software';
+        $campanha->categoriaDescricao = 'To make hardware accessible for humans';
+        $campanha->categoriaEstado = 1;
+        $campanha->save();
+
+        $campanha = new Categoria();
+        $campanha->categoriaNome = 'Storage';
+        $campanha->categoriaDescricao = 'To save all of your precious art';
+        $campanha->categoriaEstado = 1;
+        $campanha->save();
+
+        $campanha = new Categoria();
+        $campanha->categoriaNome = 'Network';
+        $campanha->categoriaDescricao = 'To connect and share data between two or more computers';
+        $campanha->categoriaEstado = 1;
+        $campanha->save();
         //endregion
 
 
         //region table_categoria_child
+        //region Categoria: Components
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'CPU';
+        $campanha->childDescricao = 'Central Processing Unit';
+        $campanha->categoria_idcategorias = 1;
+        $campanha->childEstado = 1;
+        $campanha->save();
 
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Fans';
+        $campanha->childDescricao = 'To cool down your computer';
+        $campanha->categoria_idcategorias = 1;
+        $campanha->childEstado = 1;
+        $campanha->save();
+
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Motherboards';
+        $campanha->childDescricao = 'Usually used to build your own computer';
+        $campanha->categoria_idcategorias = 1;
+        $campanha->childEstado = 1;
+        $campanha->save();
         //endregion
 
+        //region Categoria: Software
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Operating System';
+        $campanha->childDescricao = 'To run your software on top of your hardware';
+        $campanha->categoria_idcategorias = 2;
+        $campanha->childEstado = 1;
+        $campanha->save();
 
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Image Editing';
+        $campanha->childDescricao = 'To edit images';
+        $campanha->categoria_idcategorias = 2;
+        $campanha->childEstado = 1;
+        $campanha->save();
+
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Video Editing';
+        $campanha->childDescricao = 'To edit videos';
+        $campanha->categoria_idcategorias = 2;
+        $campanha->childEstado = 1;
+        $campanha->save();
+        //endregion
+
+        //region Categoria: Storage
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'USB Sticks';
+        $campanha->childDescricao = 'Small and mobile';
+        $campanha->categoria_idcategorias = 3;
+        $campanha->childEstado = 1;
+        $campanha->save();
+
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Internal';
+        $campanha->childDescricao = 'For the inside of you computer';
+        $campanha->categoria_idcategorias = 3;
+        $campanha->childEstado = 1;
+        $campanha->save();
+
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'External';
+        $campanha->childDescricao = 'Same as internal storage but more practical';
+        $campanha->categoria_idcategorias = 3;
+        $campanha->childEstado = 1;
+        $campanha->save();
+        //endregion
+
+        //region Categoria: Network
+        $campanha = new CategoriaChild();
+        $campanha->childNome = 'Switch';
+        $campanha->childDescricao = 'To manage all the connections';
+        $campanha->categoria_idcategorias = 4;
+        $campanha->childEstado = 1;
+        $campanha->save();
+        //endregion
+        //endregion
+
+/*
         //region table_produto
-
+        //region Categoria: Components
+        //region CategoriaChild: CPU
+        $campanha = new Produto();
+        $campanha->produtoNome = 'CPU';
+        $campanha->produtoCodigo = 'Central Processing Unit';
+        $campanha->produtoDataCriacao = 'Central Processing Unit';
+        $campanha->produtoStock = 'Central Processing Unit';
+        $campanha->produtoPreco = 'Central Processing Unit';
+        $campanha->produtoMarca = 'Central Processing Unit';
+        $campanha->produtoDescricao1 = 'Central Processing Unit';
+        $campanha->produtoDescricao2 = 'Central Processing Unit';
+        $campanha->produtoDescricao3 = 'Central Processing Unit';
+        $campanha->produtoDescricao4 = 'Central Processing Unit';
+        $campanha->produtoDescricao5 = 'Central Processing Unit';
+        $campanha->produtoDescricao6 = 'Central Processing Unit';
+        $campanha->produtoDescricao7 = 'Central Processing Unit';
+        $campanha->categoria_child_id = 1;
+        $campanha->save();
         //endregion
 
+        //region CategoriaChild: Fans
+        $campanha = new Produto();
+        $campanha->produtoNome = 'CPU';
+        $campanha->produtoCodigo = 'Central Processing Unit';
+        $campanha->produtoDataCriacao = 'Central Processing Unit';
+        $campanha->produtoStock = 'Central Processing Unit';
+        $campanha->produtoPreco = 'Central Processing Unit';
+        $campanha->produtoMarca = 'Central Processing Unit';
+        $campanha->produtoDescricao1 = 'Central Processing Unit';
+        $campanha->produtoDescricao2 = 'Central Processing Unit';
+        $campanha->produtoDescricao3 = 'Central Processing Unit';
+        $campanha->produtoDescricao4 = 'Central Processing Unit';
+        $campanha->produtoDescricao5 = 'Central Processing Unit';
+        $campanha->produtoDescricao6 = 'Central Processing Unit';
+        $campanha->produtoDescricao7 = 'Central Processing Unit';
+        $campanha->categoria_child_id = 1;
+        $campanha->save();
+        //endregion
+
+        //region CategoriaChild: Motherboards
+        $campanha = new Produto();
+        $campanha->produtoNome = 'CPU';
+        $campanha->produtoCodigo = 'Central Processing Unit';
+        $campanha->produtoDataCriacao = 'Central Processing Unit';
+        $campanha->produtoStock = 'Central Processing Unit';
+        $campanha->produtoPreco = 'Central Processing Unit';
+        $campanha->produtoMarca = 'Central Processing Unit';
+        $campanha->produtoDescricao1 = 'Central Processing Unit';
+        $campanha->produtoDescricao2 = 'Central Processing Unit';
+        $campanha->produtoDescricao3 = 'Central Processing Unit';
+        $campanha->produtoDescricao4 = 'Central Processing Unit';
+        $campanha->produtoDescricao5 = 'Central Processing Unit';
+        $campanha->produtoDescricao6 = 'Central Processing Unit';
+        $campanha->produtoDescricao7 = 'Central Processing Unit';
+        $campanha->categoria_child_id = 1;
+        $campanha->save();
+        //endregion
+        //endregion
+        //endregion
+*/
 
         //region table_userdata
 
@@ -154,7 +345,7 @@ class m181125_162407_createDefaultTableData extends Migration
         //region table_reparacao
 
         //endregion
-    }*/
+    }
 
     public function down()
     {
