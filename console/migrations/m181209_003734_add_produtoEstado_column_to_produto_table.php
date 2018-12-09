0aa3e42b6b73c995ac5ee2dc39e69e5b5a69c14e
PieterAp <2170365@my.ipleiria.pt>
2018-12-09 05:07:17 +0000

    Create m181209_003734_add_produtoEstado_column_to_produto_table.php

---
+++ console/migrations/m181209_003734_add_produtoEstado_column_to_produto_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding estado to table `produto`.
+ */
+class m181209_003734_add_produtoEstado_column_to_produto_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('produto', 'produtoEstado', $this->tinyInteger()->defaultValue('1'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('produto', 'produtoEstado');
+    }
+}
