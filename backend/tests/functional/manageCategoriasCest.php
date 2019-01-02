125e887d87a617f16148ba6030bf6758dfa29d50
PieterAp <2170365@my.ipleiria.pt>
2019-01-02 20:15:34 +0000

    Tests - manageCategoriasCest Funcional + produtoTest Unit

---
+++ backend/tests/functional/manageCategoriasCest.php
@@ -0,0 +1,133 @@
+<?php namespace backend\tests\functional;
+use backend\tests\FunctionalTester;
+
+class manageCategoriasCest
+{
+    protected $formCategory = '#form-category';
+
+    public function manageCategorias(FunctionalTester $I)
+    {
+        //Login into backend in order to manage CATEGORIA
+        codecept_debug("\n\n .::Login::. \n");
+
+        $I->amOnPage('/site/login');
+        $I->fillField('Username', 'pieter');
+        $I->fillField('Password', 'pieterpieter');
+
+        $I->click('login-button');
+        $I->see('Logout (pieter)', 'form button[type=submit]');
+        $I->dontSeeLink('Login');
+        $I->dontSeeLink('Signup');
+        $I->see('Welcome to backend!', 'h1');
+
+
+        //Creates new CATEGORIA
+        codecept_debug("\n\n .::Create CATEGORIA::. \n");
+        $I->amOnPage('/site/index');
+        $I->see('Welcome to backend!', 'h1');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->click('Create Category', 'a');
+        $I->see('Create Category', 'h1');
+
+        $I->submitForm('#form-category', []);
+        $I->expectTo('see validations errors');
+        $I->see('Name', 'label');
+        $I->see('Name cannot be blank');
+
+        $I->submitForm($this->formCategory, [
+            'Categoria[categoriaNome]' => 'DIY',
+            'Categoria[categoriaDescricao]' => 'Do it yourself',
+        ]);
+
+        $I->seeRecord('common\models\Categoria', [
+            'categoriaNome' => 'DIY',
+            'categoriaDescricao' => 'Do it yourself',
+            'categoriaEstado' => 0,
+        ]);
+
+
+        //Edits previously created CATEGORIA
+        codecept_debug("\n\n .::Edit CATEGORIA::. \n");
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->see('DIY', 'strong');
+        $I->see('Edit', '#editDIY');
+        $I->click('Edit', '#editDIY');
+        $I->see('Update Category: DIY', 'h1');
+
+        $I->submitForm($this->formCategory, [
+            'Categoria[categoriaNome]' => 'Gaming',
+            'Categoria[categoriaDescricao]' => 'Gaming Products',
+        ]);
+
+        $I->seeRecord('common\models\Categoria', [
+            'categoriaNome' => 'Gaming',
+            'categoriaDescricao' => 'Gaming Products',
+            'categoriaEstado' => 0,
+        ]);
+
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->see('Gaming', 'strong');
+
+
+        //Hides CATEGORIA
+        codecept_debug("\n\n .::Hide CATEGORIA::. \n");
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->see('Gaming', 'strong');
+        $I->see('Unhide Cat', '#hideGaming');
+        $I->click('Unhide Cat', '#hideGaming');
+
+        $I->seeRecord('common\models\Categoria', [
+            'categoriaNome' => 'Gaming',
+            'categoriaDescricao' => 'Gaming Products',
+            'categoriaEstado' => 1,
+        ]);
+
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->see('Gaming', 'strong');
+        $I->see('Hide Cat', '#hideGaming');
+        $I->click('Hide Cat', '#hideGaming');
+
+
+        //Deletes CATEGORIA
+        codecept_debug("\n\n .::Delete CATEGORIA::. \n");
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->see('Gaming', 'strong');
+        $I->see('Delete', '#deleteGaming');
+        $I->click('Delete', '#deleteGaming');
+
+        $I->dontSeeRecord('common\models\Categoria', [
+            'categoriaNome' => 'Gaming',
+            'categoriaDescricao' => 'Gaming Products',
+            'categoriaEstado' => 0,
+        ]);
+
+        $I->amOnPage('/site/index');
+        $I->see('Categories', 'h2');
+        $I->click('Manage', 'a');
+        $I->see('Categories', 'h1');
+        $I->dontSee('Gaming', 'strong');
+
+
+
+
+
+        codecept_debug("\n\n\n\n .::CATEGORIA CHILD::. \n");
+    }
+}
