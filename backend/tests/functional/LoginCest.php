e18f5e2095ad5cf08fb85209cf388da185d91bb2
pedromig123 <pedropedro_miguel@hotmail.com>
2019-01-02 17:58:06 +0000

    small changes

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -17,15 +17,6 @@ class LoginCest
      * @see \Codeception\Module\Yii2::loadFixtures()
      * @return array
      */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'login_data.php'
-            ]
-        ];
-    }
     
     /**
      * @param FunctionalTester $I
@@ -33,11 +24,11 @@ class LoginCest
     public function loginUser(FunctionalTester $I)
     {
         $I->amOnPage('/site/login');
-        $I->fillField('Username', 'pedroa');
-        $I->fillField('Password', '123123');
+        $I->fillField('Username', 'pedro');
+        $I->fillField('Password', 'pedropedro');
         $I->click('login-button');
 
-        $I->see('Logout (pedroa)', 'form button[type=submit]');
+        $I->see('Logout (pedro)', 'form button[type=submit]');
         $I->dontSeeLink('Login');
         $I->dontSeeLink('Signup');
     }
