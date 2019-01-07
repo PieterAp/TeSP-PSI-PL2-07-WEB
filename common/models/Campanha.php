50a62f921a0bb1907c0314e856e6fea35991b058
PieterAp <2170365@my.ipleiria.pt>
2019-01-07 15:15:54 +0000

    Messaging MQTT

--- common/models/Campanha.php
+++ common/models/Campanha.php
@@ -64,6 +64,81 @@ class Campanha extends \yii\db\ActiveRecord
     }
 
     /**
+     * Function will come in action after model gets saved
+     * @param bool $insert
+     * @param array $changedAttributes
+     */
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        $id = $this->idCampanha;
+        $nome = $this->campanhaNome;
+        $dataInicio = $this->campanhaDataInicio;
+        $descricao = $this->campanhaDescricao;
+        $dataFim = $this->campanhaDataFim;
+
+        $myObj = new \stdClass();
+        $myObj->id = $id;
+        $myObj->nome = $nome;
+        $myObj->dataInicio = $dataInicio;
+        $myObj->descricao = $descricao;
+        $myObj->dataFim = $dataFim;
+
+        $myJSON = json_encode($myObj);
+
+        if($insert)
+        {
+            $this->FazPublish('INSERT',$myJSON);
+        }
+        else
+        {
+            $this->FazPublish('UPDATE',$myJSON);
+        }
+    }
+
+
+    /**
+     * Function will come in action after model gets deleted
+     */
+    public function afterDelete()
+    {
+        parent::afterDelete();
+
+        $camp_id = $this->idCampanha;
+        $myObj = new \stdClass();
+        $myObj->id = $camp_id;
+        $myJSON = json_encode($myObj);
+
+        $this->FazPublish('DELETE', $myJSON);
+    }
+
+    /**
+     * Function will publish a message to a broker channel of Mosquitto MQTT
+     */
+
+    public function FazPublish($canal, $msg)
+    {
+        $server = "127.0.0.1";
+        $port = 1883;
+        $username = "";
+        $password = "";
+        $client_id = "phpMQTT-publisher";
+        $mqtt = new \backend\mosquitto\phpMQTT($server, $port, $client_id);
+
+        if ($mqtt->connect(true, NULL, $username, $password))
+        {
+            $mqtt->publish($canal, $msg, 0);
+            $mqtt->close();
+        }
+        else
+        {
+            file_put_contents("debug.output","Time out!");
+        }
+    }
+
+    /**
      * @return \yii\db\ActiveQuery
      */
     public function getProdutocampanhas()
