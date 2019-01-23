ca93c6e504050b5d1cb8bf7b4f5bc280c8900f44
pedromig123 <pedropedro_miguel1@hotmail.com>
2019-01-23 13:57:39 +0000

    small fix

--- frontend/views/produto/view.php
+++ frontend/views/produto/view.php
@@ -89,8 +89,8 @@ use yii\helpers\Url;
                         <?= $produtoCategoriaChild['childNome'] ?>
                     </a>
             </div>
-            <div class="wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content lol">
-                <h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4 ">
+            <div class="wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content">
+                <h5 class="js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4 lol">
                     Description
                     <i id="minuss" class="down-mark fs-12 color1 fa fa-minus dis-none" aria-hidden="true"></i>
                     <i id="pluss" class="up-mark fs-12 color1 fa fa-plus" aria-hidden="true"></i>
